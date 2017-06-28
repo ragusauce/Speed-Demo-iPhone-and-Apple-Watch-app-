6453d2054019677bf72686917e429ee4f80ca32e
Shane Ragusa <shaneragusa@96.211.21.198.tigernet.wifi.dyn.clemson.edu>
2017-06-28 13:11:45 -0400

    Initial Commit

---
+++ Speed Demo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Speed Demo WatchKit Extension
+//
+//  Created by Shane Ragusa on 6/28/17.
+//  Copyright © 2017 Shane Ragusa. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
